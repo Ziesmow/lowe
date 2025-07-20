e52c3817945ceb521bc36c885737b5b99234053f
Ziemowit <Ziesmow@proton.me>
2025-07-20 20:21:17 +0200

    added remove

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
+const lwe = @import("lwe_lib");
 const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
-const String = @import("./zig-string.zig").String;
 const ArrayList = std.ArrayList;
 
 pub fn main() !void {
@@ -15,17 +15,19 @@ pub fn main() !void {
     while (true) {
         try stdout.print(">>> ", .{});
         const user = try stdin.readUntilDelimiter(&bufferin, '\n');
-        if (CompareString(user, "help")) {
+        if (lwe.CompareString(user, "help")) {
             try HelpSombody();
-        } else if (CompareString(user, "exit")) {
+        } else if (lwe.CompareString(user, "exit")) {
             try stdout.print("quiting a program with save", .{});
             std.process.exit(0);
-        } else if (CompareString(user, "add")) {
+        } else if (lwe.CompareString(user, "add")) {
             try AddGame(&list);
-        } else if (CompareString(user, "list")) {
+        } else if (lwe.CompareString(user, "list")) {
             try ListTool(&list);
-        } else if (CompareString(user, "change")) {
+        } else if (lwe.CompareString(user, "change")) {
             try ChangeOrder(&list);
+        } else if (lwe.CompareString(user, "remove")) {
+            try RemoveGame(&list);
         } else {
             try stdout.print("don't recognize it \n", .{});
         }
@@ -39,6 +41,28 @@ pub fn AddGame(list: *ArrayList([]const u8)) !void {
     const allocator = list.allocator;
     const copy = try allocator.dupe(u8, newgamename);
     try list.append(copy);
+}
+
+pub fn RemoveGame(list: *ArrayList([]const u8)) !void {
+    if (list.items.len == 0) {
+        try stdout.print("nothing to remove", .{});
+        return;
+    }
+    var bufferin: [1024]u8 = undefined;
+    try ListTool(list);
+    try stdout.print("write what of nodes you want to remove: \n", .{});
+    const removegame = try stdin.readUntilDelimiter(&bufferin, '\n');
+    if (lwe.CompareString(removegame, "no")) {
+        try stdout.print("ok, i removed nothing \n", .{});
+        return;
+    }
+    for (list.items, 0..) |name, index| {
+        if (lwe.CompareString(removegame, name)) {
+            _ = list.orderedRemove(index);
+            return;
+        }
+    }
+    try stdout.print("ups, i don't recognize what you want to remove", .{});
 }
 
 pub fn ListTool(list: *ArrayList([]const u8)) !void {
@@ -75,17 +99,6 @@ pub fn ChangeOrder(list: *ArrayList([]const u8)) !void {
     try ListTool(list);
 }
 
-pub fn CompareString(a: []const u8, b: []const u8) bool {
-    if (a.len != b.len)
-        return false;
-    if (a.ptr == b.ptr)
-        return true;
-    for (a, b) |aElem, bElem| {
-        if (aElem != bElem) return false;
-    }
-    return true;
-}
-
 pub fn HelpSombody() !void {
-    try stdout.print("add -- for a new game, \nexit -- exit the loop \nlist -- for a list, \nchange -- for changing order of games, \nhelp -- you are reading this\n", .{});
+    try stdout.print("add -- for a new game, \nexit -- exit the loop \nlist -- for a list, \nchange -- for changing order of games, \nhelp -- you are reading this\nremove -- removes node that you input\n", .{});
 }
